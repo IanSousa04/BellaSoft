d12e3d382b6f3f6e0fd754fa765432715f5e6694
Ian Sousa <ian04sousa@gmail.com>
2025-03-06 02:02:23 -0300

    Atualiza configuração do Supabase para usar variáveis de ambiente do Vite

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -2,16 +2,16 @@ import { createClient } from '@supabase/supabase-js';
 import { Database } from '../types/supabase';
 
 // Environment variables should be in .env file
-// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-// const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// const supabaseUrl = process.env.SUPABASE_URL;
+// const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
-console.log('SUPABASE_URL:', process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log('SUPABASE_ANON_KEY:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+console.log('SUPABASE_URL:', process.env.VITE_SUPABASE_URL);
+console.log('SUPABASE_ANON_KEY:', process.env.VITE_SUPABASE_URL);
 
-console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
-console.log('SUPABASE_ANON_KEY:', process.env.SUPABASE_ANON_KEY);
+console.log('SUPABASE_URL:', import.meta.env.VITE_SUPABASE_URL);
+console.log('SUPABASE_ANON_KEY:', import.meta.env.VITE_SUPABASE_ANON_KEY);
 
 
 if (!supabaseUrl || !supabaseAnonKey) {
